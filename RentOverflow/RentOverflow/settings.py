4e93adac490d0f3178b409e1de81d533bea4dd70
Hung <b.hungng21@gmail.com>
2023-07-26 00:21:38 +0100

    updated

--- RentOverflow/RentOverflow/settings.py
+++ RentOverflow/RentOverflow/settings.py
@@ -151,3 +151,12 @@ MEDIA_ROOT = BASE_DIR / 'static/media'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 DATA_UPLOAD_MAX_NUMBER_FIELDS = 10240
+
+# Email sending
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = '587'
+EMAIL_USE_TLS = 'True'
+
+EMAIL_HOST_USER = 'sudokaname070899@gmail.com'
+EMAIL_HOST_PASSWORD = 'pvcnetzkmrhicvut'
--- RentOverflow/account/views.py
+++ RentOverflow/account/views.py
@@ -1,6 +1,18 @@
 from django.shortcuts import render, redirect 
 from .forms import CreateUserForm, LoginForm
 
+# import models
+from django.contrib.auth.models import User
+
+# for registration
+from django.contrib.sites.shortcuts import get_current_site
+from .token import user_tokenizer_generate
+
+from django.template.loader import render_to_string
+from django.core.mail import send_mail
+from django.utils.encoding import force_bytes, force_str    
+from django.utils.http import urlsafe_base64_decode, urlsafe_base64_encode
+
 # for login
 from django.contrib.auth.models import auth 
 from django.contrib.auth import authenticate, login, logout
@@ -17,6 +29,27 @@ def register(request):
             user = form.save()
             user.is_active = False 
             user.save()
+
+            # Email verification setup
+            current_site = get_current_site(request)
+            
+            subject = 'Account verification email'
+            
+            message = render_to_string('account/registration/email_verification.html', {
+                'user': user,
+                'domain': current_site.domain,
+                'uid':urlsafe_base64_encode(force_bytes(user.pk)),
+                'token': user_tokenizer_generate.make_token(user),
+            })
+
+            send_mail(
+                subject,
+                message,
+                "sudokaname070899@gmail.com",
+                [user.email],
+                fail_silently=False,
+            )
+            return redirect('email_verification_sent')
     context = {'form': form}
     
     return render(request, 'account/registration/register.html', context=context)
@@ -41,3 +74,26 @@ def my_login(request):
     }
     
     return render(request, 'account/general/my_login.html',context=context_dict)
+
+def email_verification(request, uidb64, token):
+    uid = force_str(urlsafe_base64_decode(uidb64))
+    user = User.objects.get(pk=uid)
+    
+    if user and user_tokenizer_generate.check_token(user, token):
+        user.is_active = True 
+        user.save()
+        return redirect('email_verification_success')
+    else:
+        return redirect('email_verification_failed')
+    
+
+def email_verification_sent(request):
+    return render(request, 'account/registration/email_verification_sent.html', context={})
+
+
+def email_verification_success(request):
+    return render(request, 'account/registration/email_verification_success.html', context={})
+ 
+
+def email_verification_failed(request):
+    return render(request, 'account/registration/email_verification_failed.html', context={})
